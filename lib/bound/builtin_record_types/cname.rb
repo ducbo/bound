0d8d01997431dd1bdfe5045a54ae1769d2eb8623
Tran Dinh Duc <ducbo@yahoo.com>
2020-08-12 12:28:47 +0700

    Update cname.rb

--- lib/bound/builtin_record_types/cname.rb
+++ lib/bound/builtin_record_types/cname.rb
@@ -14,13 +14,27 @@ module Bound
       end
 
       def validate(hash, errors)
-        if hash['name'].blank?
-          errors << "Canonical Name must be provided"
+        if hash['name']
+          if hash['name'].starts_with?('') && hash['name'].ends_with?('.')
+            errors << "Data does not need be enclosed in quotes - these are added when exported"
+          end
         end
       end
 
       def serialize(hash)
-        hash['name']
+        data = hash['name']
+        if data.blank?
+          '@'
+        else
+          total_length = data.size
+          parts, remainder = total_length.divmod(255)
+          parts = parts + (remainder > 0 ? 1 : 0)
+          array = []
+          parts.times do |i|
+            array << '' + data[i * 255, 255] + '.'
+          end
+          array.join(' ')
+        end
       end
 
       def deserialize(string)
